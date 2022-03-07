6c22d6e28b20aeab0b7138ef044d043b93c72358
zanedr <zanedanielrussell@gmail.com>
2022-03-07 14:45:56 -0700

    major updates with new share omdal

--- shareModal/ShareModalSteps/index.js
+++
@@ -1,8 +0,0 @@
-export { default as WhatToDo } from "./WhatToDo/WhatToDo";
-export { default as AbleToAccess } from "./AbleToAccess";
-export { default as WhichWallet } from "./WhichWallet";
-export { default as AssetWallet } from "./AssetWallet/AssetWallet";
-export { default as DAOMembers } from "./DAOMembers";
-export { default as AccessCreated } from "./AccessCreated";
-export { default as SelectTokens } from "./SelectTokens";
-export { default as ChoosePOAP } from "./ChoosePOAP";
